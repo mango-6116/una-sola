3b2a58b72abd6f5b3dd1d4c04b94b053a27e8ada
mango-6116 <131622766+mango-6116@users.noreply.github.com>
2023-04-26 00:16:11 -0400

    -el nombre otra vez

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chess-peg'`, () => {
+  it(`should have as title 'una-sola'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    //expect(app.title).toEqual('chess-peg');
+    //expect(app.title).toEqual('una-sola');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('chess-peg app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('una-sola app is running!');
   });
 });
